936f30d40cdcfdda6c14080acfc382057850a5bb
Yevasee <e.saidyakov@yandex.ru>
2025-11-08 01:04:06 +0500

    Добавьте файлы проекта.

---
+++ SaidyakovTelenet/PageAddEdit.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SaidyakovTelenet
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddEdit.xaml
+    /// </summary>
+    public partial class PageAddEdit : Page
+    {
+        private SUBSCRIBER _currentSubscriber = new SUBSCRIBER();
+        public PageAddEdit(SUBSCRIBER SelectedSubscriber)
+        {
+            InitializeComponent();
+            if (SelectedSubscriber != null)
+            {
+                _currentSubscriber = SelectedSubscriber;
+            }
+            else
+            {
+                //TextBoxSubscriberId.Visibility = Visibility.Collapsed;
+                TextBoxSubscriberId.Foreground = new SolidColorBrush(SystemColors.ControlColor);
+                TextBoxSubscriberId.Background = new SolidColorBrush(SystemColors.ControlColor);
+                _currentSubscriber.SUBSCRIBER_LASTNAME = "Иванов";
+                _currentSubscriber.SUBSCRIBER_FIRSTNAME = "Иван";
+                _currentSubscriber.SUBSCRIBER_PATRONYMIC = "Иванович";
+                _currentSubscriber.SUBSCRIBER_ADDRESS = "Стройбург, ул. Рабочих, 42";
+                _currentSubscriber.SUBSCRIBER_CONNECTION_DATE = DateTime.Today;
+                _currentSubscriber.SUBSCRIBER_STATUS = "Активен";
+            }
+            switch (_currentSubscriber.SUBSCRIBER_STATUS)
+            {
+                case "Активен": ComboBoxStatusOfSubscriber.SelectedIndex = 0; break;
+                case "Приостановлен": ComboBoxStatusOfSubscriber.SelectedIndex = 1; break;
+                case "Заблокирован": ComboBoxStatusOfSubscriber.SelectedIndex = 2; break;
+            }
+            DataContext = _currentSubscriber;
+        }
+
+        private void ComboBoxStatusOfSubscriber_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            switch (ComboBoxStatusOfSubscriber.SelectedIndex)
+            {
+                case 0: _currentSubscriber.SUBSCRIBER_STATUS = "Активен"; break;
+                case 1: _currentSubscriber.SUBSCRIBER_STATUS = "Приостановлен"; break;
+                case 2: _currentSubscriber.SUBSCRIBER_STATUS = "Заблокирован"; break;
+            }
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentSubscriber.SUBSCRIBER_LASTNAME))
+            {
+                errors.AppendLine("Укажите фамилию абонента");
+            }
+            if (string.IsNullOrWhiteSpace(_currentSubscriber.SUBSCRIBER_FIRSTNAME))
+            {
+                errors.AppendLine("Укажите имя абонента");
+            }
+            if (string.IsNullOrWhiteSpace(_currentSubscriber.SUBSCRIBER_ADDRESS))
+            {
+                errors.AppendLine("Укажите адрес абонента");
+            }
+            if (string.IsNullOrWhiteSpace(TextBoxSubscriberConnectionDate.Text))
+            {
+                errors.AppendLine("Укажите дату подключения");
+            }
+            if (ComboBoxStatusOfSubscriber.SelectedItem == null)
+            {
+                errors.AppendLine("Укажите статус абонента");
+            }
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentSubscriber.SUBSCRIBER_ID == 0)
+            {
+                TelenetDBEntities.GetContext().SUBSCRIBER.Add(_currentSubscriber);
+            }
+
+
+            try
+            {
+                TelenetDBEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                Manager.FrameBase.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var _subscriber = (sender as Button).DataContext as SUBSCRIBER;
+
+            var _subscriberStatus = _subscriber.SUBSCRIBER_STATUS;
+
+            if (_subscriberStatus.Equals("Активен") || _subscriberStatus.Equals("Приостановлен"))
+                MessageBox.Show("Невозможно выполнить удаление, так как тариф используется абонентом");
+            else
+            {
+                var _subscriberTariff = TelenetDBEntities.GetContext().TARIFF.ToList();
+                var _subscriberPhone = TelenetDBEntities.GetContext().PHONE_NUMBER.ToList();
+                var _subscriberServiceRequest = TelenetDBEntities.GetContext().SERVICE_REQUEST.ToList();
+                var _subscriberPayment = TelenetDBEntities.GetContext().PAYMENT.ToList();
+                _subscriberTariff = _subscriberTariff.
+                    Where(p => p.SUBSCRIBER_ID == _subscriber.SUBSCRIBER_ID).ToList();
+                _subscriberPhone = _subscriberPhone.
+                    Where(p => p.SUBSCRIBER_ID == _subscriber.SUBSCRIBER_ID).ToList();
+                _subscriberServiceRequest = _subscriberServiceRequest.
+                    Where(p => p.SUBSCRIBER_ID == _subscriber.SUBSCRIBER_ID).ToList();
+                _subscriberPayment = _subscriberPayment.
+                    Where(p => p.SUBSCRIBER_ID == _subscriber.SUBSCRIBER_ID).ToList();
+
+                if (_subscriberTariff.Count > 0 || _subscriberPhone.Count > 0 || _subscriberServiceRequest.Count > 0 || _subscriberPayment.Count > 0)
+                    MessageBox.Show("Невозможно выполнить удаление, так как присутствуют связанные записи");
+                else
+                {
+                    if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!",
+                        MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                    {
+                        try
+                        {
+                            TelenetDBEntities.GetContext().SUBSCRIBER.Remove(_subscriber);
+                            TelenetDBEntities.GetContext().SaveChanges();
+
+                            MessageBox.Show("Информация удалена!");
+                            Manager.FrameBase.GoBack();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message.ToString());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SaidyakovTelenet/PageSubscribers.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SaidyakovTelenet
+{
+    /// <summary>
+    /// Логика взаимодействия для PageSubscribers.xaml
+    /// </summary>
+    public partial class PageSubscribers : Page
+    {
+        List<SUBSCRIBER> listSubscribersForPage = new List<SUBSCRIBER>();
+        int countOfRecordsInTotal = 0;
+        int countOfRecordsOnPage = 0;
+        public PageSubscribers()
+        {
+            InitializeComponent();
+            ComboBoxSorter.SelectedIndex = 0;
+            ComboBoxFilter.SelectedIndex = 0;
+            UpdateSubscribers();
+        }
+        public void UpdateSubscribers()
+        {
+            listSubscribersForPage = TelenetDBEntities.GetContext().SUBSCRIBER.ToList();
+            countOfRecordsInTotal = listSubscribersForPage.Count;
+            TextBlockCountOfRecordsInTotal.Text = countOfRecordsInTotal.ToString();
+            ComboBoxSorterMaker();
+            ComboBoxFilterMaker();
+            TextBoxSearchMaker();
+
+            countOfRecordsOnPage = listSubscribersForPage.Count;
+            TextBlockCountOfRecordsOnPage.Text = countOfRecordsOnPage.ToString();
+        }
+
+        private void ComboBoxSorter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSubscribers();
+        }
+
+        private void ComboBoxFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSubscribers();
+        }
+
+        private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateSubscribers();
+        }
+
+        private void ComboBoxSorterMaker()
+        {
+            switch (ComboBoxSorter.SelectedIndex)
+            {
+                case 0: listSubscribersForPage = listSubscribersForPage.ToList(); break;
+                case 1: listSubscribersForPage = listSubscribersForPage.OrderBy(p => p.subscriberFullName).ToList(); break;
+                case 2: listSubscribersForPage = listSubscribersForPage.OrderByDescending(p => p.subscriberFullName).ToList(); break;
+            }
+
+            listViewSubscribers.ItemsSource = listSubscribersForPage;
+        }
+
+        private void ComboBoxFilterMaker()
+        {
+            switch (ComboBoxFilter.SelectedIndex)
+            {
+                case 0: listSubscribersForPage = listSubscribersForPage.ToList(); break;
+                case 1: listSubscribersForPage = listSubscribersForPage.Where(p => (p.SUBSCRIBER_STATUS.Equals("Активен"))).ToList(); break;
+                case 2: listSubscribersForPage = listSubscribersForPage.Where(p => (p.SUBSCRIBER_STATUS.Equals("Приостановлен"))).ToList(); break;
+                case 3: listSubscribersForPage = listSubscribersForPage.Where(p => (p.SUBSCRIBER_STATUS.Equals("Заблокирован"))).ToList(); break;
+            }
+
+            listViewSubscribers.ItemsSource = listSubscribersForPage;
+        }
+        
+        private void TextBoxSearchMaker()
+        {
+            string[] searchText = TextBoxSearch.Text.ToLower().Split(' ');
+            for (int i = 0; i < searchText.Length; i++)
+            {
+                listSubscribersForPage = listSubscribersForPage.Where(p => (p.SUBSCRIBER_ID.ToString().ToLower().Contains(searchText[i])
+                                                                   || p.subscriberFullName.ToLower().Contains(searchText[i])
+                                                                   || p.SUBSCRIBER_ADDRESS.ToLower().Contains(searchText[i])
+                                                                   || p.SUBSCRIBER_CONNECTION_DATE.ToString().ToLower().Contains(searchText[i])
+                                                                   || p.SUBSCRIBER_STATUS.ToLower().Contains(searchText[i])
+                                                                   )).ToList();
+            }
+
+            listViewSubscribers.ItemsSource = listSubscribersForPage;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.FrameBase.Navigate(new PageAddEdit(null));
+        }
+
+        private void ChangeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.FrameBase.Navigate(new PageAddEdit((sender as Button).DataContext as SUBSCRIBER));
+        }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                TelenetDBEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                listViewSubscribers.ItemsSource = TelenetDBEntities.GetContext().SUBSCRIBER.ToList();
+                UpdateSubscribers();
+            }
+        }
+    }
+}
